6df768d13c19ba60fb6c423051617221e7e3c5b0
Atuahene Enoch <atuahene.enoch777@gmail.com>
2022-09-09 07:33:04 -0700

    put not print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
-/*Here is the entry point*/
+/**
+ * main - Entry point
+ *
+ * Description: puts some text to screen''
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }

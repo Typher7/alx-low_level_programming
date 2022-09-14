90a89b46cb915c5e0ddeeb9c2afcadd8a6bcccd9
Atuahene Enoch <atuahene.enoch777@gmail.com>
2022-09-13 21:14:33 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <uinstd>
+#include "main.h"
+
+/**
+ * main - Check description
+ * Description: It prints the alphabet in lowercase, followed by a new line.
+ * Return: 0.
+ */
+int main(void)
+{
+char text[30] = "abcdefghijklmnopqrstuvwxyz";
+int i
+for (i=0; i<27; i++)
+{
+_putchar(text[i]);
+}
+_putchar('\n');
+}
